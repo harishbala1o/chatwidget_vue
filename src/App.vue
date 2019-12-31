3dd607c7d2dfdfb21628c1ed64a7aaaec87061c7
harishbala1o <harishbala1o@gmail.com>
2019-12-31 17:52:25 +0530

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="How to add chat widget in a vue.js app"/>
   </div>
 </template>
 
