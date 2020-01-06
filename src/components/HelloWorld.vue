f2c03ea8302bca62cd4c6d354b44da6f08c11fa0
harishbala1o <harishbala1o@gmail.com>
2020-01-06 19:02:31 +0530

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,10 +6,19 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+    methods: {
+    },
+    mounted (){
+      (function(d, m){
+        var kommunicateSettings = {"appId":"kommunicate-support","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+        window.kommunicate = m; m._globals = kommunicateSettings;
+      })(document, window.kommunicate || {});
+    },
+    data: function(){
+    }
 }
 </script>
 
